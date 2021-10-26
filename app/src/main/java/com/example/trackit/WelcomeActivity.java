cc27c1fa01360fb954d878f247e524586cf38c26
David Parise <parise@ualberta.ca>
2021-10-26 03:56:21 -0600

    Removed bottom navigation activity

--- app/src/main/java/com/example/trackit/WelcomeActivity.java
+++ app/src/main/java/com/example/trackit/WelcomeActivity.java
@@ -2,7 +2,10 @@ package com.example.trackit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        final Button loginButton = findViewById(R.id.login_button);
+        final Button registerButton = findViewById(R.id.register_button);
+    }
+
+    public void goToLogin(View view) {
+        // add Intent here
+    }
+
+    public void goToRegister(View view) {
+        // add Intent here
     }
 }
