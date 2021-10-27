84398ef9685797d2944d71fb8f83eadc9f5fe850
David Parise <parise@ualberta.ca>
2021-10-26 20:41:45 -0600

    Modified AddHabitActivity.java

--- app/src/main/java/com/example/trackit/AddHabitActivity.java
+++ app/src/main/java/com/example/trackit/AddHabitActivity.java
@@ -3,12 +3,45 @@ package com.example.trackit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class AddHabitActivity extends AppCompatActivity {
+
+    EditText addHabitTitle;
+    EditText addHabitReason;
+    Button addHabitButton;
+    Button cancelButton;
+    DatePicker datePicker;
+    TextView selectedDate;
+    String addStartDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_habit);
+        selectedDate = findViewById(R.id.add_start_date_text);
+        datePicker = findViewById(R.id.select_start_date);
+        addHabitButton = findViewById(R.id.button_add_habit);
+        cancelButton = findViewById(R.id.button_cancel);
+        addHabitReason = findViewById(R.id.add_reason);
+        addHabitTitle = findViewById(R.id.add_title);
+    }
+
+    public void selectDate(View view) {
+        // Retrieve and set selected start date
+        String date = Integer.toString(datePicker.getDayOfMonth());
+        String month = Integer.toString(datePicker.getMonth() + 1);
+        if (Integer.valueOf(month) < 10) {
+            month = "0" + month;
+        }
+        String year = Integer.toString(datePicker.getYear());
+        addStartDate = date + "-" + month + "-" + year;
+        selectedDate.setText(addStartDate);
     }
 }
