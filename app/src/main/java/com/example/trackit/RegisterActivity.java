0048aeba9acb990e243cf4da2fb12ad1ea6a5687
David Parise <parise@ualberta.ca>
2021-10-27 16:40:34 -0600

    Modified gradle files

--- app/src/main/java/com/example/trackit/RegisterActivity.java
+++ app/src/main/java/com/example/trackit/RegisterActivity.java
@@ -1,33 +1,69 @@
 package com.example.trackit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();;
+    final CollectionReference collectionReference = db.collection("Cities");
     EditText inputUsername;
     EditText inputPassword;
-    String registerUsername;
-    String registerPassword;
+    Button registerButton;
+    final String TAG = "Sample";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-    }
 
-    public void signUp (View view) {
-        // Retrieve data and register user
         inputUsername = findViewById(R.id.register_username);
         inputPassword = findViewById(R.id.register_password);
+        registerButton = findViewById(R.id.register);
 
-        registerUsername = inputUsername.getText().toString();
-        registerPassword = inputPassword.getText().toString();
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String registerUsername = inputUsername.getText().toString();
+                final String registerPassword = inputPassword.getText().toString();
+
+                HashMap<String, String> data = new HashMap<>();
+                if (registerUsername.length()>0 && registerPassword.length()>0) {
+                    data.put("Password", registerPassword);
+
+                    collectionReference
+                            .document(registerUsername)
+                            .set(data)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Log.d(TAG, "Data has been added successfully!");
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG, "Data could not be added!" + e.toString());
+                                }
+                            });
+
+                    inputUsername.setText("");
+                    inputPassword.setText("");
+                }
+            }
+        });
     }
 }
