b3e8504383c49450388df7cc99562fcb47a3bc61
David Parise <parise@ualberta.ca>
2021-10-27 13:32:55 -0600

    Refactored files

--- app/src/main/java/com/example/trackit/TodaysHabitsActivity.java
+++ app/src/main/java/com/example/trackit/TodaysHabitsActivity.java
@@ -1,23 +1,21 @@
 package com.example.trackit;
 
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
 
 import java.util.ArrayList;
 
-public class TodaysHabitActivity extends AppCompatActivity {
+public class TodaysHabitsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_todays_habit);
+        setContentView(R.layout.activity_todays_habits);
         ArrayList<String> days = new ArrayList<String>();
         days.add("Wednesday");
         Habit habit = new Habit("sleep at night", "get enough sleep", "12-10-2021", days);
@@ -30,7 +28,7 @@ public class TodaysHabitActivity extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
-                Intent intent = new Intent(TodaysHabitActivity.this, EditHabitActivity.class);
+                Intent intent = new Intent(TodaysHabitsActivity.this, EditHabitActivity.class);
                 intent.putExtra("habit", (Parcelable) habit);
                 startActivity(intent);
             }
