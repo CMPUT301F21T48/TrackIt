afd15ebe59fd8a2c21231dac61c711576f238155
Akarsh654 <56489645+Akarsh654@users.noreply.github.com>
2021-10-29 07:27:53 -0600

    Added Todays Activity, CustomList
    CustomList.java
    TodaysActivity.java
    habits.xml

---
+++ app/src/main/java/com/example/trackit/CustomList.java
@@ -0,0 +1,53 @@
+package com.example.trackit;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class CustomList extends ArrayAdapter<Habit> {
+
+    private final ArrayList<Habit> habits;
+    private final Context context;
+
+    // constructor
+    public CustomList(Context context, ArrayList<Habit> habits) {
+        super(context, 0, habits);
+        this.habits = habits;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        //return super.getView(position, convertView, parent);
+        View view = convertView;
+
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.habit, parent, false);
+        }
+
+        Habit habit = habits.get(position);
+
+        TextView habitTitle = view.findViewById(R.id.habit_title_display);
+        TextView habitRepeat = view.findViewById(R.id.habit_repeat_display);
+        TextView habitReason = view.findViewById(R.id.habit_reason_display);
+
+        habitTitle.setText(habit.getTitle());
+        String repeatDays = "";
+        for (int i = 0; i < habit.getRepeatDays().size(); i++){
+            repeatDays = repeatDays + " " + habit.getRepeatDays().get(i);
+        }
+        habitRepeat.setText("Repeat: " + repeatDays);
+        habitReason.setText("Reason: " + habit.getReason());
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/trackit/TodaysHabitsActivity.java
+++ app/src/main/java/com/example/trackit/TodaysHabitsActivity.java
@@ -2,11 +2,20 @@ package com.example.trackit;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -14,15 +23,31 @@ import java.util.ArrayList;
 public class TodaysHabitsActivity extends AppCompatActivity {
 
     Intent intent;
+    ListView habitList;
+    ArrayAdapter<Habit> habitAdapter;
+    ArrayList<Habit> habitDataList;
+
+    CustomList customList;
+    User user;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    CollectionReference collectionReference;
+    final String TAG = "Sample";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todays_habits);
 
-        User user = (User) getIntent().getSerializableExtra("User");
+        user = (User) getIntent().getSerializableExtra("User");
+        collectionReference = db.collection("Users").document(user.getUsername()).collection("Habits");
 
-        Button addButton = findViewById(R.id.add_button);
-        Button editButton = findViewById(R.id.edit_button);
+        habitList = findViewById(R.id.habit_list);
+        habitDataList = new ArrayList<>();
+        habitAdapter = new CustomList(this, habitDataList);
+        habitList.setAdapter(habitAdapter);
+
+        FloatingActionButton addButton = findViewById(R.id.add_button);
 
         addButton.setOnClickListener(new View.OnClickListener()
         {
@@ -32,6 +57,26 @@ public class TodaysHabitsActivity extends AppCompatActivity {
                 intent = new Intent(TodaysHabitsActivity.this, AddHabitActivity.class);
                 intent.putExtra("User", (Serializable) user);
                 startActivity(intent);
+            }
+        });
+
+        collectionReference.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException error) {
+                habitDataList.clear();
+                for(QueryDocumentSnapshot doc: queryDocumentSnapshots) {
+                    String ID = doc.getId();
+                    String title = (String) doc.getData().get("title");
+                    String reason = (String) doc.getData().get("reason");
+                    String startDate = (String) doc.getData().get("startDate");
+                    ArrayList<String> repeatDays = (ArrayList<String>) doc.getData().get("repeatDays");
+//                    Integer progress = (Integer) doc.getData().get("progress");
+                    Habit newHabit = new Habit(title, reason, startDate, repeatDays);
+//                    newHabit.setProgress(progress);
+                    newHabit.setHabitID(ID);
+                    habitDataList.add(newHabit);
+                }
+                habitAdapter.notifyDataSetChanged();
             }
         });
 
