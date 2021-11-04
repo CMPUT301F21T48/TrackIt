a195aeafb5fa791a204eaad5a96bef3a15d1b4cf
Akarsh654 <56489645+Akarsh654@users.noreply.github.com>
2021-11-04 09:39:18 -0600

    Added comments and JavaDocs for all HabitActivities, Login, Register and User

--- app/src/main/java/com/example/trackit/User.java
+++ app/src/main/java/com/example/trackit/User.java
@@ -13,18 +13,36 @@ public class User implements Serializable {
 
     public User() {}
 
+    /**
+     * this returns the user name
+     * @return return the user name
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * sets the user name to the registered username
+     * @param registeredUsername
+     *          registered user name to set
+     */
     public void setUsername(String registeredUsername) {
         username = registeredUsername;
     }
 
+    /**
+     * returns the password of the user
+     * @return
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * sets the password of the user to the registered password
+     * @param registeredPassword
+     *      password the user registered with
+     */
     public void setPassword(String registeredPassword) {
         password = registeredPassword;
     }
