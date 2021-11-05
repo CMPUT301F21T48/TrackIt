107dfee688bc1aaafa08ea01be086396e01ccda3
David Parise <parise@ualberta.ca>
2021-11-05 05:29:02 -0600

    Added JavaDocs

--- app/src/main/java/com/example/trackit/UserSearchAdapter.java
+++ app/src/main/java/com/example/trackit/UserSearchAdapter.java
@@ -1,23 +1,21 @@
 package com.example.trackit;
 
+import static android.content.ContentValues.TAG;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
-
-/**
- * This is the search adapter for user search.
- */
-public class UserSearchAdapter extends RecyclerView.Adapter<UserSearchAdapter.ViewHolder>{
-  
-/**
- * This is the search adapter for the user search
- */
 
 public class UserSearchAdapter extends RecyclerView.Adapter<UserSearchAdapter.ViewHolder> implements Filterable {
 
@@ -54,58 +52,52 @@ public class UserSearchAdapter extends RecyclerView.Adapter<UserSearchAdapter.Vi
         holder.userName.setText(userNames.get(position));
 
     }
- 
-////  For use in Part 4
-//    public Filter getFilter(){
-//        return filter;
-//    }
-//
-//    Filter filter = new Filter() { // filter is a problem area because it doesnt seem to execute filter results
-//        @Override
-//        protected FilterResults performFiltering(CharSequence charSequence) {
-//            Log.d(TAG, charSequence.toString());
-//            List<String> filteredResults = new ArrayList<>();
-//            Integer count = 0;
-//            if(charSequence.toString().isEmpty()){
-//                filteredResults.addAll(userNamesComplete);
-//
-//                Log.d(TAG, charSequence.toString());
-//            }
-//            else{
-//                for(String check : userNamesComplete){
-//                    if(check.toLowerCase().contains(charSequence.toString().toLowerCase())){
-//                        filteredResults.add(check);
-//                        Log.d(TAG, "performFiltering:  " + count++);
-//                    }
-//                    Log.d(TAG, "performFiltering:  " + count++);
-//                }
-//            }
-//            for(String check: filteredResults){
-//                Log.d(TAG, check);
-//            }
-//            FilterResults filtered = new FilterResults();
-//            filtered.values = filteredResults;
-//            return  filtered;
-//        }
-//
-//        @Override
-//        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
-//            userNames.clear();
-//            Log.d(TAG, "publishResults: Cleared");
-//            userNames.addAll((Collection<? extends String>) filterResults.values);
-//            notifyDataSetChanged();
-//        }
-//    };
+
+    public Filter getFilter(){
+        return filter;
+    }
+
+    Filter filter = new Filter() { // filter is a problem area because it doesnt seem to execute filter results
+        @Override
+        protected FilterResults performFiltering(CharSequence charSequence) {
+            Log.d(TAG, charSequence.toString());
+            List<String> filteredResults = new ArrayList<>();
+            Integer count = 0;
+            if(charSequence.toString().isEmpty()){
+                filteredResults.addAll(userNamesComplete);
+
+                Log.d(TAG, charSequence.toString());
+            }
+            else{
+                for(String check : userNamesComplete){
+                    if(check.toLowerCase().contains(charSequence.toString().toLowerCase())){
+                        filteredResults.add(check);
+                        Log.d(TAG, "performFiltering:  " + count++);
+                    }
+                    Log.d(TAG, "performFiltering:  " + count++);
+                }
+            }
+            for(String check: filteredResults){
+                Log.d(TAG, check);
+            }
+            FilterResults filtered = new FilterResults();
+            filtered.values = filteredResults;
+            return  filtered;
+        }
+
+        @Override
+        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+            userNames.clear();
+            Log.d(TAG, "publishResults: Cleared");
+            userNames.addAll((Collection<? extends String>) filterResults.values);
+            notifyDataSetChanged();
+        }
+    };
 
     @Override
     public int getItemCount() {
         return userNames.size();
     }
-
-    /**
-     * ViewHolder is custom class that is used to find what position item is clicked
-     * so that we can return it and find the actual item that has been clicked
-     **/
 
     class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
