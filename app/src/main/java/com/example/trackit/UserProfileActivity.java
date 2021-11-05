1b3b85dae965f228cff3dcd6f3b7cfc57bbc248a
David Parise <parise@ualberta.ca>
2021-11-05 05:57:29 -0600

    Revert UserProfileActivity

--- app/src/main/java/com/example/trackit/UserProfileActivity.java
+++ app/src/main/java/com/example/trackit/UserProfileActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -16,6 +17,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
@@ -27,7 +29,11 @@ import com.google.firebase.firestore.QuerySnapshot;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-
+/**
+ * This is the activity to display user profile.
+ * It displays the users habits, number of individuals they are following and
+ * the number of users that follow them
+ */
 public class UserProfileActivity extends AppCompatActivity {
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -42,18 +48,23 @@ public class UserProfileActivity extends AppCompatActivity {
     ListView habitList;
     ArrayAdapter<Habit> habitAdapter;
     ArrayList<Habit> habitDataList;
-
+    TextView emptyMessage;
+    FloatingActionButton addButton;
+    Habit habit;
+    String followStatus;
     @SuppressLint("SetTextI18n")
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
+        emptyMessage = findViewById(R.id.no_habit_message);
         followButton = findViewById(R.id.followButton);
         userNameView = findViewById(R.id.userNameView);
         userFollowers = findViewById(R.id.followerCount);
         userFollowing = findViewById(R.id.followingCount);
 
+        addButton = findViewById(R.id.add_habit);
         habitList = findViewById(R.id.habitList);
         habitDataList = new ArrayList<>();
         habitAdapter = new HabitCustomList(this, habitDataList);
@@ -64,10 +75,19 @@ public class UserProfileActivity extends AppCompatActivity {
 
         Boolean exists = false;
 
-
         userNameView.setText(chosenUserName);
+        currentUser.setUsername(currentUserName);
 
-        getDatafromFB(new AsyncCall() {
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UserProfileActivity.this, AddHabitActivity.class);
+                intent.putExtra("User", currentUser);
+                startActivity(intent);
+            }
+        });
+
+        getDataFromFB(new AsyncCall() {
             @Override
             public void onCallBack(Integer finalCheckValue) {
                 followers = finalCheckValue;
@@ -76,7 +96,7 @@ public class UserProfileActivity extends AppCompatActivity {
 
         }, chosenUserName, "Followers");
 
-        getDatafromFB(new AsyncCall() {
+        getDataFromFB(new AsyncCall() {
             @Override
             public void onCallBack(Integer finalCheckValue) {
                 following = finalCheckValue;
@@ -110,8 +130,25 @@ public class UserProfileActivity extends AppCompatActivity {
                         newHabit.setProgress();
                         habitDataList.add(newHabit);
                     }
+                    if (habitDataList.size() == 0) {
+                        emptyMessage.setVisibility(View.VISIBLE);
+                        emptyMessage.setText("You have not added any habit(s).");
+                    }
                     habitAdapter.notifyDataSetChanged();
                 };
+            });
+
+            habitList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> arg0, View view, int position, long arg3) {
+                    habitList.setSelection(position);
+                    habit = (Habit) habitList.getItemAtPosition(position);
+                    Intent intent = new Intent(UserProfileActivity.this, ViewHabitActivity.class);
+                    intent.putExtra("User", currentUser);
+                    intent.putExtra("HabitID", habit.getHabitID());
+                    intent.putExtra("Habit", habit);
+                    startActivity(intent);
+                }
             });
         }
         else{
@@ -124,9 +161,39 @@ public class UserProfileActivity extends AppCompatActivity {
                             public void onCallBack(Boolean exists) {
                                 if(exists){
                                     followButton.setText("Unfollow");
+
+                                    collectionReference.document(chosenUserName).collection("Habits").addSnapshotListener(new EventListener<QuerySnapshot>() {
+                                        @Override
+                                        public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException error) {
+                                            habitDataList.clear();
+                                            for (QueryDocumentSnapshot doc : queryDocumentSnapshots) {
+                                                String ID = doc.getId();
+                                                String title = (String) doc.getData().get("title");
+                                                String reason = (String) doc.getData().get("reason");
+                                                String startDate = (String) doc.getData().get("startDate");
+                                                ArrayList<String> repeatDays = (ArrayList<String>) doc.getData().get("repeatDays");
+
+                                                int numDone = (int) ((long) doc.getData().get("numDone"));
+                                                int numNotDone = (int) ((long) doc.getData().get("numNotDone"));
+                                                Habit newHabit = new Habit(title, reason, startDate, repeatDays);
+                                                newHabit.setHabitID(ID);
+                                                newHabit.setNumDone(numDone);
+                                                newHabit.setNumNotDone(numNotDone);
+                                                newHabit.setProgress();
+                                                habitDataList.add(newHabit);
+                                            }
+                                            if (habitDataList.size() == 0) {
+                                                emptyMessage.setVisibility(View.VISIBLE);
+                                                emptyMessage.setText("This user does not have any added habit(s).");
+                                            }
+                                            habitAdapter.notifyDataSetChanged();
+                                        }
+                                    });
                                 }
                                 else{
                                     followButton.setText("Requested");
+                                    emptyMessage.setVisibility(View.VISIBLE);
+                                    emptyMessage.setText("You do not have permission to view this user's habits.");
                                 }
                             }
                         }, "Followers", chosenUserName, currentUserName);
@@ -143,34 +210,11 @@ public class UserProfileActivity extends AppCompatActivity {
                                 startActivity(intent);
                             };
                         });
-
-                        collectionReference.document(chosenUserName).collection("Habits").addSnapshotListener(new EventListener<QuerySnapshot>() {
-                            @Override
-                            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException error) {
-                                habitDataList.clear();
-                                for (QueryDocumentSnapshot doc : queryDocumentSnapshots) {
-                                    String ID = doc.getId();
-                                    String title = (String) doc.getData().get("title");
-                                    String reason = (String) doc.getData().get("reason");
-                                    String startDate = (String) doc.getData().get("startDate");
-                                    ArrayList<String> repeatDays = (ArrayList<String>) doc.getData().get("repeatDays");
-
-                                    int numDone = (int) ((long) doc.getData().get("numDone"));
-                                    int numNotDone = (int) ((long) doc.getData().get("numNotDone"));
-                                    Habit newHabit = new Habit(title, reason, startDate, repeatDays);
-                                    newHabit.setHabitID(ID);
-                                    newHabit.setNumDone(numDone);
-                                    newHabit.setNumNotDone(numNotDone);
-                                    newHabit.setProgress();
-                                    habitDataList.add(newHabit);
-                                }
-                                habitAdapter.notifyDataSetChanged();
-                            };
-                        });
-
                     }
                     else{
                         followButton.setText("Follow");
+                        emptyMessage.setVisibility(View.VISIBLE);
+                        emptyMessage.setText("You do not have permission to view this user's habits.");
                         followButton.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View view) {
@@ -255,7 +299,7 @@ public class UserProfileActivity extends AppCompatActivity {
         void onCallBack(Integer finalCheckValue);
     }
 
-    public void getDatafromFB(AsyncCall asyncCall, String Username, String CollectionList){
+    public void getDataFromFB(AsyncCall asyncCall, String Username, String CollectionList){
         final Integer[] Value = {0};
         db.collection("Users").document(Username).collection(CollectionList)
                 .get()
@@ -277,6 +321,4 @@ public class UserProfileActivity extends AppCompatActivity {
                     }
                 });
     }
-
-//    public Boolean existsInFB(String )
 }
