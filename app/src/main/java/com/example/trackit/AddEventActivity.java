f6af466047e3a09487354c6de6c12c5dd84fcdca
Akarsh654 <56489645+Akarsh654@users.noreply.github.com>
2021-11-24 18:28:52 -0700

    Camera launching and clicking photo in AddEventActivity.java

--- app/src/main/java/com/example/trackit/AddEventActivity.java
+++ app/src/main/java/com/example/trackit/AddEventActivity.java
@@ -9,6 +9,8 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -16,6 +18,8 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -23,8 +27,11 @@ import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.firebase.client.authentication.Constants;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -39,10 +46,13 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.GeoPoint;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
@@ -63,7 +73,15 @@ public class AddEventActivity extends AppCompatActivity implements OnMapReadyCal
     private final LatLng defaultLocation = new LatLng(-33.8523341, 151.2106085);
     private static final int DEFAULT_ZOOM = 15;
     private Marker currentMarker;
+    private String encodedPhoto;
+    private Double MAX_IMAGE_BYTE = 275.0;
+    private Context ImageContext;
+    //Request codes
+    public static final int REQUEST_CODE = 100;
+    public static final int CAMERA_REQUEST = 9999;
+    private static final int MY_CAMERA_PERMISSION_CODE = 100;
 
+    private ImageView imageView;
 
     // The entry point to the Places API.
     List<Address> addresses;
@@ -73,6 +91,8 @@ public class AddEventActivity extends AppCompatActivity implements OnMapReadyCal
     final CollectionReference collectionReference = db.collection("Users");
     User user;
     Habit habit;
+    Button photo_button;
+    //ImageView photo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,6 +101,8 @@ public class AddEventActivity extends AppCompatActivity implements OnMapReadyCal
         user = (User) getIntent().getSerializableExtra("User");
         habit = (Habit) getIntent().getSerializableExtra("Habit");
         comment = findViewById(R.id.add_comment);
+        photo_button = findViewById(R.id.button_photo);
+        imageView = findViewById(R.id.photo);
 //
 //        Places.initialize(getApplicationContext(), BuildConfig.map_);
 //        placesClient = Places.createClient(this);
@@ -91,6 +113,98 @@ public class AddEventActivity extends AppCompatActivity implements OnMapReadyCal
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        photo_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent photoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (photoIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(photoIntent, CAMERA_REQUEST);
+                    //finish();
+                }
+            }
+        });
+    }
+
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+//    {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        if (requestCode == MY_CAMERA_PERMISSION_CODE)
+//        {
+//            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+//            {
+//                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
+//                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+//                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+//            }
+//            else
+//            {
+//                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+//            }
+//        }
+//    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        ImageContext = getApplicationContext();
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK && data != null)  {
+            //Bitmap photo = (Bitmap) data.getExtras().get("data");
+            //imageView.setImageBitmap(photo);
+
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            encodeBitmapAndSaveToFirebase(imageBitmap);
+
+            imageView.setImageBitmap(image);
+            imageView.setVisibility(View.VISIBLE);
+
+        }
+    }
+
+    public void encodeBitmapAndSaveToFirebase(Bitmap bitmap) {
+        this.image = bitmap;
+
+        if (bitmap.getByteCount() > MAX_IMAGE_BYTE) {
+            for (int i = 0; i < 3; ++i) {
+                bitmap = resizeImage(bitmap);
+//                    Log.i("HabitUpDEBUG", "Resized to " + String.valueOf(photo.getByteCount()) + " bytes.");
+                if (bitmap.getByteCount() <= MAX_IMAGE_BYTE) {
+                    break;
+                }
+            }
+        }
+
+        if (bitmap.getByteCount() <= MAX_IMAGE_BYTE) {
+
+            this.image = bitmap;
+
+            ByteArrayOutputStream byteArrayOS = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOS);
+            this.encodedPhoto = Base64.encodeToString(byteArrayOS.toByteArray(), Base64.DEFAULT);
+
+        } else {
+            throw new IllegalArgumentException("Image file must be less than or equal to " +
+                    String.valueOf(MAX_IMAGE_BYTE) + " bytes.");
+        }
+
+
+    }
+
+    private Bitmap resizeImage(Bitmap bitmap) {
+        double scaleFactor = 0.95;
+        return Bitmap.createScaledBitmap(bitmap, (int) (bitmap.getWidth() * scaleFactor), (int) (bitmap.getHeight() * scaleFactor), true);
+    }
+
+    /**
+     * Set photo and encodedphoto to null, clearing them
+     */
+    public void deletePhoto() {
+        this.image= null;
+        this.encodedPhoto = null;
     }
 
 
